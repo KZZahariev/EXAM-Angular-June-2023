b9ba8430e1fc7fd0749a1c1714323b9c1ec611e5
KZZahariev <dsad_1993@abv.bg>
2023-08-19 12:50:40 +0300

    .

--- Rest-api/config/config.js
+++ Rest-api/config/config.js
@@ -9,7 +9,7 @@ const config = {
     production: {
         port: process.env.PORT || 3000,
         dbURL: 'mongodb+srv://KZZahariev:Paris9602@cluster0.uz1syes.mongodb.net/?retryWrites=true&w=majority', //process.env.DB_URL_CREDENTIALS, //
-        origin: ['http://localhost:5555', 'http://localhost:4200', 'https://exam-angular-june-2023.vercel.app/error']
+        origin: ['http://localhost:5555', 'http://localhost:4200', 'https://exam-angular-june-2023.vercel.app']
     }
 };
 
